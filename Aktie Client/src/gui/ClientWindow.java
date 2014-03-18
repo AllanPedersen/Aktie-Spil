aec33319ea6086ada185686c22432dfdcae2f365
Chris <chn91@me.com>
2014-03-18 10:20:04 +0100

    Small change to client window

--- Aktie Client/src/gui/ClientWindow.java
+++ Aktie Client/src/gui/ClientWindow.java
@@ -13,8 +13,7 @@ public class ClientWindow extends JFrame {
 		super();
 		this.setTitle("Aktiespil");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(800, 600);
-		
+		this.setSize(800, 600);		
 		this.setVisible(true);
 	}
 
