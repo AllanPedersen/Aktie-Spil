f021d0b4dea1d40aae60cd254eae645b6df54426
Chris <chn91@me.com>
2014-03-18 10:18:29 +0100

    Added empty window for client

--- Aktie Client/src/gui/ClientWindow.java
+++ Aktie Client/src/gui/ClientWindow.java
@@ -3,5 +3,19 @@ package gui;
 import javax.swing.JFrame;
 
 public class ClientWindow extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ClientWindow() {
+		super();
+		this.setTitle("Aktiespil");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(800, 600);
+		
+		this.setVisible(true);
+	}
 
 }
