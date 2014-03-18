f434005978fa2083d6fd7678d27db126f53e2cb4
Chris <chn91@me.com>
2014-03-18 11:19:03 +0100

    Added setup window for clients

---
+++ Aktie Client/src/gui/SetupWindow.java
@@ -0,0 +1,41 @@
+package gui;
+
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+
+public class SetupWindow extends JFrame implements MouseListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public SetupWindow() {
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+
+}
