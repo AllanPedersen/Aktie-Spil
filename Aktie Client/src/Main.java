8a3b934bf300ddac10f326d36d4e83386472a577
Chris <chn91@me.com>
2014-03-18 10:59:25 +0100

    Small changes

--- Aktie Client/src/Main.java
+++ Aktie Client/src/Main.java
@@ -2,8 +2,20 @@ import gui.ClientWindow;
 
 
 public class Main {
+	
+	private static String lcOSName = System.getProperty("os.name").toLowerCase();
+	private static boolean IS_MAC = lcOSName.startsWith("mac os x");
 
 	public static void main(String[] args) {
+		// Mac specific tasks
+		if (IS_MAC) {
+			// Put menubar at the top of the screen
+			System.setProperty("apple.laf.useScreenMenuBar", "true");
+			
+			// Set title of app
+			System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Aktiespil");
+		}
+		
 		System.out.println("Hello, this is the client");
 		new ClientWindow();
 	}
--- Aktie Server/src/gui/ServerWindow.java
+++ Aktie Server/src/gui/ServerWindow.java
@@ -3,5 +3,10 @@ package gui;
 import javax.swing.JFrame;
 
 public class ServerWindow extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
 }
