c73c571288392edf3aaafb451d802a2ac299d5fb
Chris <chn91@me.com>
2014-04-07 11:50:38 +0200

    Added Player class to Client

---
+++ Aktie Client/src/logic/Player.java
@@ -0,0 +1,53 @@
+package logic;
+
+public class Player {
+	
+	private String name, email, ip;
+	private int port;
+	
+	public Player(String name, String email, String ip, int port) {
+		this.setName(name);
+		this.setEmail(email);
+		this.setIp(ip);
+		this.setPort(port);
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	public String toString() {
+		return this.getName();
+	}
+	
+
+
+}
