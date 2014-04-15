e2f81e656d393f3a2cbbc222b990959976f180a5
Chris <chn91@me.com>
2014-04-15 12:15:44 +0200

    Added class to handle XML data from Yahoo stock API

---
+++ Aktie Client/src/xml/Parser.java
@@ -0,0 +1,17 @@
+package xml;
+
+import java.util.ArrayList;
+
+import logic.Stock;
+
+public class Parser {
+	
+	public static ArrayList<Stock> getAllStocks() {
+		return null;
+	}
+	
+	public static double getValueFromSymbol() {
+		return 0;
+	}
+
+}
