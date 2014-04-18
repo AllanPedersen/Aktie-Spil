262cef6dca86c174870168a5c2d4ee374d9bfefe
Chris <chn91@me.com>
2014-04-18 12:35:59 +0200

    Updated parser so that no zero values should occur

--- Aktie Client/src/xml/Parser.java
+++ Aktie Client/src/xml/Parser.java
@@ -17,7 +17,7 @@ public class Parser {
 		String[] stocks = {"AAPL", "GOOG", "YHOO", "TWTR", "FB", "GS", "CSCO", "MSFT", "JPM", "C", 
 				"GE", "F", "GM", "NOK", "BBRY", "ORCL", "T", "HPQ", "AAL", "FOXA", "JCP", "VZ", "WMT",
 				"MCD", "DIS", "AXP", "BA", "KO"};
-		
+
 		// Create the URL string with all the above stocks
 		String url = "";
 
@@ -29,21 +29,21 @@ public class Parser {
 
 		url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22"
 				+ url + "%22)&env=store://datatables.org/alltableswithkeys";
-		
+
 		System.out.println(url);
 
 		ArrayList<Stock> stcks = new ArrayList<Stock>();
-		
+
 		// Fetch XML and parse into objects
 		try {
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(new URL(url).openStream());
-			
+
 			doc.getDocumentElement().normalize();
 
 			NodeList nList = doc.getElementsByTagName("quote");
-			
+
 			for (int temp = 0; temp < nList.getLength(); temp++) {
 
 				Node nNode = nList.item(temp);
@@ -51,22 +51,26 @@ public class Parser {
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
 					Element eElement = (Element) nNode;
-					System.out.println("Symbol : " + eElement.getAttribute("symbol"));
-					System.out.println("Name : " + eElement.getElementsByTagName("Name").item(0).getTextContent());
-					System.out.println("Value : " + eElement.getElementsByTagName("AskRealtime").item(0).getTextContent());
 
-					Stock tempStock = new Stock(eElement.getElementsByTagName("Name").item(0).getTextContent(), 
-							Double.parseDouble(eElement.getElementsByTagName("AskRealtime").item(0).getTextContent()));
-					tempStock.setSymbol(eElement.getAttribute("symbol"));
-					stcks.add(tempStock);
+					if (Double.parseDouble(eElement.getElementsByTagName("AskRealtime").item(0).getTextContent()) == 0) {
+						Stock tempStock = new Stock(eElement.getElementsByTagName("Name").item(0).getTextContent(), 
+								Double.parseDouble(eElement.getElementsByTagName("Bid").item(0).getTextContent()));
+						tempStock.setSymbol(eElement.getAttribute("symbol"));
+						stcks.add(tempStock);
+					} else {
+						Stock tempStock = new Stock(eElement.getElementsByTagName("Name").item(0).getTextContent(), 
+								Double.parseDouble(eElement.getElementsByTagName("AskRealtime").item(0).getTextContent()));
+						tempStock.setSymbol(eElement.getAttribute("symbol"));
+						stcks.add(tempStock);
+					}
 				}
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
+
 		return stcks;
 	}
 
@@ -75,7 +79,7 @@ public class Parser {
 				+ "select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22"+symbol+"%22)"
 				+ "&env=store://datatables.org/alltableswithkeys";
 
-		
+
 		try {
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -93,7 +97,11 @@ public class Parser {
 
 					Element eElement = (Element) nNode;
 
-					return Double.parseDouble(eElement.getElementsByTagName("AskRealtime").item(0).getTextContent());
+					if (Double.parseDouble(eElement.getElementsByTagName("AskRealtime").item(0).getTextContent()) == 0) {
+						return Double.parseDouble(eElement.getElementsByTagName("Bid").item(0).getTextContent());
+					} else {
+						return Double.parseDouble(eElement.getElementsByTagName("AskRealtime").item(0).getTextContent());
+					}
 				}
 			}
 		} catch (Exception e) {
